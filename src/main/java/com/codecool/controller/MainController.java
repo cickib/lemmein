c9f5eee2ac4782db3ff26e553044927adf0b3dcf
cickib <bellakocsis06@gmail.com>
2017-05-09 13:53:33 +0200

    flats added to template

--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,10 +1,19 @@
 package com.codecool.controller;
 
+import com.codecool.model.Flat;
+import com.codecool.repository.FlatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private FlatRepository flatRepository;
 
     @GetMapping(value = "/")
     public String index() {
@@ -12,7 +21,9 @@ public class MainController {
     }
 
     @GetMapping(value = "/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        List<Flat> flats = flatRepository.findAll();
+        model.addAttribute("flats", flats);
         return "dashboard";
     }
 }
