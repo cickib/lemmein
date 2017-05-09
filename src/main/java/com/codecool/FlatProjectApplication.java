754edfcb9736c79321d7403388008022f1a30fc3
cickib <bellakocsis06@gmail.com>
2017-05-09 12:18:17 +0200

    basic routes serve templates

--- src/main/java/com/codecool/FlatProjectApplication.java
+++ src/main/java/com/codecool/FlatProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlatProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlatProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlatProjectApplication.class, args);
+    }
 }
--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -2,14 +2,17 @@ package com.codecool.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping(value = "/")
-    @ResponseBody
     public String index() {
-        return "OK";
+        return "index";
+    }
+
+    @GetMapping(value = "/dashboard")
+    public String dashboard() {
+        return "dashboard";
     }
 }
