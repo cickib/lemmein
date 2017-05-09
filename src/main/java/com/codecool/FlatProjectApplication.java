d467ac12cf367a1cf781160973b68a85d2568bd5
cickib <bellakocsis06@gmail.com>
2017-05-09 11:18:30 +0200

    home root rendered ok

---
+++ src/main/java/com/codecool/FlatProjectApplication.java
@@ -0,0 +1,12 @@
+package com.codecool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatProjectApplication.class, args);
+	}
+}
