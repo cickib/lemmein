46c91dedbe21e8edf58292619a685b90bfb0baae
cickib <bellakocsis06@gmail.com>
2017-05-11 14:55:42 +0200

    clickable urls

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
     $('#flatsTable').DataTable();
+
+    $('.dataTable').on('click', 'tbody td', function () {
+        var url = this.textContent;
+        var clickableHu = ".hu";
+        var clickableCom = ".com";
+        if (containsPrefix(url, clickableHu) || containsPrefix(url, clickableCom)) {
+            window.location.href = fixUrl(url);
+        }
+    })
 });
+
+var containsPrefix = function (url, prefix) {
+    return url.indexOf(prefix) >= 0;
+};
+
+var fixUrl = function (url) {
+    var prefix = "http://www.";
+    var prefixS = "https://www.";
+    if (!(containsPrefix(url, prefix) || containsPrefix(url, prefixS))) {
+        url = prefix + url;
+    }
+    return url;
+};
