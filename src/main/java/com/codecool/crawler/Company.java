2e1e6371f6bedd6bfc49bce31b4d3d30b0b441e2
cickib <bellakocsis06@gmail.com>
2017-05-20 16:28:23 +0200

    javadoc added to factory related classes

--- src/main/java/com/codecool/crawler/Company.java
+++ src/main/java/com/codecool/crawler/Company.java
@@ -1,6 +1,8 @@
 package com.codecool.crawler;
 
-
+/**
+ * Flat rental sites' names represented as enum. Corresponds to Crawler interface implementations
+ */
 public enum Company {
     ALBERLETHU, INGATLANROBOT
 }
--- src/main/java/com/codecool/crawler/CrawlerFactory.java
+++ src/main/java/com/codecool/crawler/CrawlerFactory.java
@@ -4,9 +4,17 @@ package com.codecool.crawler;
 import com.codecool.crawler.crawlers.AlberletHuCrawler;
 import com.codecool.crawler.crawlers.IngatlanRobotCrawler;
 
-
+/**
+ * Based on given information, generates objects of a class implementing Crawler interface
+ */
 public class CrawlerFactory {
 
+    /**
+     * Factory method
+     *
+     * @param company enum representation of website name
+     * @return specified implementation of Crawler
+     */
     public static Crawler getCrawler(Company company) {
         switch (company) {
             case ALBERLETHU:
