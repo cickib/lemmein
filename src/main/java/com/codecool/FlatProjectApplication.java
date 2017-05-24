090ae06264c359290b5b8875be6f7bea963cb383
cickib <bellakocsis06@gmail.com>
2017-05-24 11:21:32 +0200

    extra data stored about flats, repo collects them in date order

--- src/main/java/com/codecool/FlatProjectApplication.java
+++ src/main/java/com/codecool/FlatProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlatProjectApplication {
-    //TODO logfile
     public static void main(String[] args) {
         SpringApplication.run(FlatProjectApplication.class, args);
     }
--- src/main/java/com/codecool/repository/FlatRepository.java
+++ src/main/java/com/codecool/repository/FlatRepository.java
@@ -4,7 +4,11 @@ import com.codecool.model.Flat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FlatRepository extends JpaRepository<Flat, Integer> {
     boolean existsByAdUrl(String url);
+
+    List<Flat> findAllByOrderByDate();
 }
