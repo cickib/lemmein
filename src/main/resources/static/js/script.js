614137d51b28fdfaa2ef397247d30056b0008934
cickib <bellakocsis06@gmail.com>
2017-05-24 20:07:10 +0200

    initial searchbar + base angular stuff added - uses jquery atm

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,7 +4,44 @@ $(document).ready(function () {
     $(".dataTable").on("click mousedown", "tbody td", function () {
         openHref(this.textContent);
     });
+
+    $("#searchBtn").click(function () {
+
+        var rentFrom = $("#rentFrom").val();
+        var rentTo = $("#rentTo").val();
+        var sizeFrom = $("#sizeFrom").val();
+        var sizeTo = $("#sizeTo").val();
+        $.ajax({
+            method: "POST",
+            url: "/search",
+            dataType: "json",
+            data: JSON.stringify({
+                "rentFrom": rentFrom,
+                "rentTo": rentTo,
+                "sizeFrom": sizeFrom,
+                "sizeTo": sizeTo,
+                "district": getChecked("district"),
+                "site": getChecked("site")
+            }),
+            contentType: "application/json; charset=utf-8"
+        });
+    });
 });
+
+var getChecked = function (name) {
+    var checked = [];
+    $("input[name='" + name + "']").each(function () {
+        var id = this.id;
+        if ($(createSelector(id)).prop("checked")) {
+            checked.push(id)
+        }
+    });
+    return checked;
+};
+
+var createSelector = function (id) {
+    return "#" + id;
+};
 
 var openHref = function (url) {
     if (isLink(url)) {
