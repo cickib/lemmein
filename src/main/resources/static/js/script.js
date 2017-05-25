76bee4a93363ba90e82711e45d5dffb6084f8e58
cickib <bellakocsis06@gmail.com>
2017-05-25 16:34:25 +0200

    searchbar included in dashboard

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,6 +5,14 @@ $(document).ready(function () {
         openHref(this.textContent);
     });
 
+    $("#dAll").click(function () {
+        checkAll(this);
+    });
+
+    $("#sAll").click(function () {
+        checkAll(this);
+    });
+
     $("#searchBtn").click(function () {
 
         var rentFrom = $("#rentFrom").val();
@@ -25,23 +33,34 @@ $(document).ready(function () {
             }),
             contentType: "application/json; charset=utf-8"
         });
+        scrollToMatches();
+        getResults();
     });
 });
+
+
+// collect checked checkboxes to array
 
 var getChecked = function (name) {
     var checked = [];
     $("input[name='" + name + "']").each(function () {
         var id = this.id;
-        if ($(createSelector(id)).prop("checked")) {
+        if ($(selectorify(id)).prop("checked")) {
             checked.push(id)
         }
     });
     return checked;
 };
 
-var createSelector = function (id) {
+
+// create jquery selector from id value
+
+var selectorify = function (id) {
     return "#" + id;
 };
+
+
+// checking, creating, opening valid links
 
 var openHref = function (url) {
     if (isLink(url)) {
@@ -55,4 +74,54 @@ var isLink = function (url) {
 
 var containsPrefix = function (url, prefix) {
     return url.indexOf(prefix) >= 0
+};
+
+
+// checkbox checking/unchecking
+
+var checkAll = function (target) {
+    var bool = $(selectorify(target.id)).prop("checked");
+    $("input[name='" + target.name + "']").each(function () {
+        var id = this.id;
+        checkCheckBox(id, bool)
+    });
+};
+
+var checkCheckBox = function (id, boolValue) {
+    $(selectorify(id)).prop("checked", boolValue);
+};
+
+
+// autoscroll to results
+
+var scrollToMatches = function () {
+    $("html, body").animate({
+        scrollTop: $("#flatsTable").offset().top
+    }, 2000);
+};
+
+
+// collect flats from backend, insert data as new row
+
+var getResults = function () {
+    $.getJSON("/results", function (data) {
+        console.log(data["flats"])
+        console.log(data["flats"].length)
+        for (var i = 0; i < data["flats"].length; i++) {
+            var flat = data["flats"][i];
+            console.log("flat " + flat)
+            $('#flatsTable > tbody:last-child').append(createFlatRecord(flat));
+        }
+    });
+};
+
+var createFlatRecord = function (flat) {
+    console.log(flat.adUrl)
+    return "<tr>" +
+        "<td >" + flat.id + "</td>" +
+        "<td>" + flat.squareMeter + "</td>" +
+        "<td>" + flat.rent + "</td>" +
+        "<td>" + flat.district + " | " + flat.address + "</td>" +
+        "<td><a class='td-url'>" + flat.adUrl + "</a></td>" +
+        "<td>" + flat.company + "</td></tr>";
 };
